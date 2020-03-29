bb9c8ca70d1538379326a168c51697d4c6828c24
QS_Specialist <romanencko.ilia@yandex.ru>
2020-03-29 15:06:25 +0300

    after try/catch

--- src/HowMuchGamePackage/HowMuchClass.java
+++ src/HowMuchGamePackage/HowMuchClass.java
@@ -6,19 +6,24 @@ import java.io.InputStreamReader;
 
 public class HowMuchClass {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));//first
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         while (true){
             System.out.println("Enter number between 1 and 100");
             int guessNumber = (int)(Math.random() * 100 + 1);
             while (true){
-                int yourNumber = Integer.parseInt(reader.readLine());
-                if (yourNumber == guessNumber) {
-                    System.out.println("You won");
-                    break;
-                }else if (yourNumber > guessNumber){
-                    System.out.println("Is is too high");
-                }else if (yourNumber < guessNumber){
-                    System.out.println("It is too low");
+                try {
+                    int yourNumber = Integer.parseInt(reader.readLine());
+
+                    if (yourNumber == guessNumber) {
+                        System.out.println("You won");
+                        break;
+                    }else if (yourNumber > guessNumber){
+                        System.out.println("Is is too high");
+                    }else if (yourNumber < guessNumber){
+                        System.out.println("It is too low");
+                    }
+                }catch (Exception e){
+                    System.out.println("It is not a number. Enter number between 1 and 100");
                 }
             }
             System.out.println("Would you like to play again? Enter y/n");
@@ -28,5 +33,6 @@ public class HowMuchClass {
                 break;
             }
         }
+        reader.close();
     }
 }
